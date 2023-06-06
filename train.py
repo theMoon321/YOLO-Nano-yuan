d7bc15d2e4beaab14061103993af1190024746f7
theMoon321 <130368802+theMoon321@users.noreply.github.com>
2023-06-06 21:04:17 +0800

    Update train.py

--- train.py
+++ train.py
@@ -6,25 +6,25 @@ from terminaltables import AsciiTable
 
 def train(model, optimizer, dataloader, epoch, opt, logger, best_mAP=0):
     for i, (images, targets) in enumerate(dataloader):
-	model.train()
+        model.train()
         # targets: [idx, class_id, x, y, h, w] in yolo format
         # idx is used to associate the bounding boxes with its image
         # skip images without bounding boxes (mainly because coco has unlabelled images) 
         if targets.size(0) == 0:
             continue
-        
+
         batches_done = len(dataloader) * epoch + i
         if opt.gpu:
             model = model.to(opt.device)
             images = Variable(images.to(opt.device))
             if targets is not None:
                 targets = Variable(targets.to(opt.device), requires_grad=False)
-        
+
         loss, detections = model.forward(images, targets)
         # detections = non_max_suppression(detections.cpu(),opt.conf_thres,opt.nms_thres)
         loss.backward()
 
-        if batches_done % opt.gradient_accumulations == 0 or i == len(dataloader)-1:
+        if batches_done % opt.gradient_accumulations == 0 or i == len(dataloader) - 1:
             optimizer.step()
             optimizer.zero_grad()
 
@@ -42,7 +42,8 @@ def train(model, optimizer, dataloader, epoch, opt, logger, best_mAP=0):
         # beautify log message
         metric_table = AsciiTable(
             metric_table_data,
-            title='[Epoch {:d}/{:d}, Batch {:d}/{:d}, Current best mAP {:4f}]'.format(epoch, opt.num_epochs, i, len(dataloader), best_mAP))
+            title='[Epoch {:d}/{:d}, Batch {:d}/{:d}, Current best mAP {:4f}]'.format(epoch, opt.num_epochs, i,
+                                                                                      len(dataloader), best_mAP))
         metric_table.inner_footing_row_border = True
         logger.print_and_write('{}\n'.format(metric_table.table))
         # print("current best mAP:" + str(best_mAP))
@@ -56,9 +57,8 @@ def train(model, optimizer, dataloader, epoch, opt, logger, best_mAP=0):
         'best_mAP': best_mAP,
     }
     save_file_path = os.path.join(opt.checkpoint_path, 'last.pth'.format(epoch))
-    torch.save(states,save_file_path)
+    torch.save(states, save_file_path)
 
     if epoch % opt.checkpoint_interval == 0:
         save_file_path = os.path.join(opt.checkpoint_path, 'epoch_{}.pth'.format(epoch))
         torch.save(states, save_file_path)
-
