5d1f0e12596042ea4c31f96bdcbd0526d1e1baec
theMoon321 <130368802+theMoon321@users.noreply.github.com>
2023-06-06 21:16:58 +0800

    Update get_dataset.py

--- data/get_dataset.py
+++ data/get_dataset.py
@@ -17,7 +17,8 @@ def get_val_dataset(opt, transforms=None):
     if opt.dataset == 'coco':
         from data.coco import COCO
         dataset = COCO(
-            opt.dataset_path, os.path.join(opt.annotation_path, opt.val_json), subset='val',
+#             opt.dataset_path, os.path.join(opt.annotation_path, opt.val_json), subset='val',
+            opt.dataset_path, opt.annotation_path, subset='val',
             image_size=opt.image_size, multi_scale=False, transforms=transforms)
     else:
         raise NotImplementedError('the dataset [%s] is not implemented' % opt.dataset_mode)
